cc82d4695ef4efc2682a2c5712f9f0d9b48368e2
Juananvg <juanantoniovillatorogarcia@gmail.com>
2024-09-25 12:42:24 +0200

    Update package documentation

--- doc/GSEMA.R
+++ doc/GSEMA.R
@@ -7,41 +7,41 @@ BiocStyle::latex()
 
 
 ###################################################
-### code chunk number 2: GSEMA.Rnw:49-51
+### code chunk number 2: GSEMA.Rnw:50-52
 ###################################################
 library(GSEMA)
 data("simulatedData")
 
 
 ###################################################
-### code chunk number 3: GSEMA.Rnw:84-86
+### code chunk number 3: GSEMA.Rnw:85-87
 ###################################################
 #List of expression matrices
 listMatrices <- list(study1Ex, study2Ex, study3Ex, study4Ex, study5Ex)
 
 
 ###################################################
-### code chunk number 4: GSEMA.Rnw:91-93
+### code chunk number 4: GSEMA.Rnw:92-94
 ###################################################
 listPhenodata <- list(study1Pheno, study2Pheno, study3Pheno, study4Pheno,
     study5Pheno)
 
 
 ###################################################
-### code chunk number 5: GSEMA.Rnw:108-109
+### code chunk number 5: GSEMA.Rnw:109-110
 ###################################################
 study1Pheno$Condition
 
 
 ###################################################
-### code chunk number 6: GSEMA.Rnw:114-116
+### code chunk number 6: GSEMA.Rnw:115-117
 ###################################################
 head(names(GeneSets))
 GeneSets[[1]]
 
 
 ###################################################
-### code chunk number 7: GSEMA.Rnw:141-155
+### code chunk number 7: GSEMA.Rnw:142-156
 ###################################################
 listMatrices <- list(study1Ex, study2Ex, study3Ex, study4Ex, study5Ex)
 listPhenodata <- list(study1Pheno, study2Pheno, study3Pheno, study4Pheno,
@@ -60,27 +60,27 @@ objectMApathSim <- createObjectMApath(
 
 
 ###################################################
-### code chunk number 8: GSEMA.Rnw:173-174
+### code chunk number 8: GSEMA.Rnw:174-175
 ###################################################
 objectMApathSim <- filteringPaths(objectMApathSim, threshold = 0.65)
 
 
 ###################################################
-### code chunk number 9: GSEMA.Rnw:208-210
+### code chunk number 9: GSEMA.Rnw:209-211
 ###################################################
 results <- metaAnalysisESpath(objectMApath = objectMApathSim,
     measure = "limma", typeMethod = "REM", missAllow = 0.3, proportionData = 1)
 
 
 ###################################################
-### code chunk number 10: GSEMA.Rnw:215-217
+### code chunk number 10: GSEMA.Rnw:216-218
 ###################################################
 results[1,]
 results[nrow(results),]
 
 
 ###################################################
-### code chunk number 11: GSEMA.Rnw:257-260
+### code chunk number 11: GSEMA.Rnw:258-261
 ###################################################
 res <- heatmapPaths(objectMA=objectMApathSim, results,
             scaling = "zscor", regulation = "all",breaks=c(-2,2),
@@ -88,7 +88,7 @@ res <- heatmapPaths(objectMA=objectMApathSim, results,
 
 
 ###################################################
-### code chunk number 12: GSEMA.Rnw:269-272
+### code chunk number 12: GSEMA.Rnw:270-273
 ###################################################
 Effects <- calculateESpath(objectMApath = objectMApathSim, measure = "limma")
 Effects[[1]][1,]
@@ -96,7 +96,7 @@ Effects[[2]][1,]
 
 
 ###################################################
-### code chunk number 13: GSEMA.Rnw:277-278
+### code chunk number 13: GSEMA.Rnw:278-279
 ###################################################
 sessionInfo()
 
