95e9127134c2ececadd75b74c66cc08ceb4c151a
Juananvg <juanantoniovillatorogarcia@gmail.com>
2024-09-27 13:29:06 +0200

    Update package test

--- inst/unitTests/MetaESpath_test.R
+++ inst/unitTests/MetaESpath_test.R
@@ -1,5 +1,5 @@
 test_ES <- function() {
-  library(GSEMA)
-  checkEqualsNumeric(metaAnalysisDE(objectMApathSim, typeMethod = "REM", 
-                    missAllow = 0.3, proportionData = 1)[1,2],
-                    0.2329579, tolerance=1.0e-6)}
+    library(GSEMA)
+    checkEqualsNumeric(metaAnalysisESpath(objectMApathSim, typeMethod = "REM", 
+      missAllow = 0.3, proportionData = 1)[1,2],
+      0.2500693, tolerance=1.0e-6)}
